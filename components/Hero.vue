3e90e0cb4766afe1770d5fdac9f65dd97423ea3a
Efe Erdem <efeerdem2001@hotmail.com>
2021-09-17 18:43:06 +0300

    first commit

---
+++ components/Hero.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="Hero container">
+        <div class="text-container">
+            <h1 class="header">Find your Fire Extinguisher</h1>
+            <p class="snippet">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste iusto illum quidem asperiores, cum nisi aperiam eius ipsa sapiente odit!</p>
+            <button class="button btn">Start Looking</button>
+        </div>
+        <div class="image" >
+        <img  width="100%" src="~assets/svg/fire-extinguisher.svg" alt="fire extinguisher">
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+    .Hero {
+        height: max-content;
+        position: relative;
+        display: flex;
+        padding: 5rem 0;
+        align-items: flex-start;
+        justify-content: space-between;
+    }
+    .text-container {
+        width: 50%;
+        margin-top: 4rem;
+    }
+    .header {
+        font-weight: 900;
+        font-size: 5rem;
+    }
+    .snippet {
+        color: grey;
+        font-size: 1.25rem;
+    }
+    .image {
+        width: 440px;
+        height: 440px;
+        margin-top: 2rem;
+    }
+    .button {
+        padding: 1rem 4rem;
+        border-radius: 100rem;
+        color: rgb(231, 81, 43);
+        background: white;
+        border: 2px solid rgb(231, 81, 43);
+        width: 20rem;
+        font-weight: 700;
+        transition: 0.2s;
+        outline: none !important;
+    }
+    .button:hover {
+        transform: scale(1.045);
+        background: rgb(231, 81, 43);
+        color: white;
+    }
+</style>
---
+++ components/LargeCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <NuxtLink :to="`/products/${card.id}`" class="card">
+    <img
+      :src="require(`@/assets/images/${card.image || 'fe1.jpg'}`)"
+      alt="fe"
+      class="image"
+    />
+    <h3 class="header">
+      {{ card.title }}
+    </h3>
+    <p class="snippet">
+      {{ card.snippet }}
+    </p>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: ["card"],
+};
+</script>
+
+<style scoped>
+.card {
+  width: 31.5%;
+  height: 25rem;
+  border: none;
+  overflow: hidden;
+  padding: 0;
+  cursor: pointer;
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+}
+.card:hover {
+  transform: scale(1.015);
+}
+.card::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 15px;
+  width: 100%;
+  height: 2px;
+  background: rgb(243, 85, 45);
+  transition: transform 200ms ease-in;
+  transform: scale(0);
+  transform-origin: left;
+}
+.card:hover::after {
+  transform: scale(1);
+}
+.image {
+  height: 65%;
+  border-radius: 0.5rem;
+}
+.header {
+  font-size: 1.15rem;
+  margin-top: 0.4rem;
+  color: black;
+    text-decoration: none;
+    transition: 0.2s;
+}
+.snippet {
+  color: grey;
+}
+</style>
